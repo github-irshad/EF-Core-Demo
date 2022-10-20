6e572e63d34d2328259f9523cd37719d6660dd0d
irshad_aj <irshadaj08@gmail.com>
2022-10-20 15:19:10 +0530

    Some ref added

--- DAL/DatabaseContext.cs
+++ DAL/DatabaseContext.cs
@@ -2,12 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data.Entity;
+using Npgsql;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace DAL
 {
     public class DatabaseContext:DbContext
     {
         Dbset<User>Users {get; set;}
+
+        protected override void onConfiguring(DbContextOptionBuilder optionBuilder)
+        {
+            if(!optionBuilder.IsConfigured)
+            {
+                optionBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=myDataBase;User Id=myUsername;Password=myPassword");
+            }
+        }
     }
 }
--- Readme.md
+++ Readme.md
@@ -1,6 +1,7 @@
 Connection String:  https://learn.microsoft.com/en-us/answers/questions/906488/read-appconfig-values-in-net-6-console.html
+  https://www.connectionstrings.com/postgresql/
 
-https://www.entityframeworktutorial.net/efcore/cli-commands-for-ef-core-migration.aspx
+Migration : https://www.entityframeworktutorial.net/efcore/cli-commands-for-ef-core-migration.aspx
 
 Ef core YT video: https://www.youtube.com/watch?v=PqFwtF9HE3Y&t=2250s
 
