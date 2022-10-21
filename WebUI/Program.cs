bc6bf4cbf1cb1b33f82612c7a33a596df7c7ac47
irshad_aj <irshadaj08@gmail.com>
2022-10-21 11:44:15 +0530

    creating dependency injedction

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,7 +1,13 @@
+
+using DAL;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseContext>();
 
 var app = builder.Build();
 
